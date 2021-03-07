3cbb4b204dd1c961e0f5f1b5cb84fc941e4f7dc3
Julien Fiegehenn <simbabque@cpan.org>
2021-03-07 22:07:41 +0000

    enable animiations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { EventsComponent } from './events/events.component';
 
@@ -13,7 +13,7 @@ import { EventsComponent } from './events/events.component';
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule,
+    BrowserAnimationsModule,
     MaterialModule
   ],
   providers: [],
