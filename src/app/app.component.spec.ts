c8be6b91653afa90582b7d282b5006e6b6e6d479
Julien Fiegehenn <simbabque@cpan.org>
2021-03-09 23:59:58 +0000

    initial pass at adding tests for app compmonent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,51 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSelectModule } from '@angular/material/select';
+import { MatSelectHarness } from '@angular/material/select/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let loader: HarnessLoader;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [AppComponent],
+        imports: [MatSelectModule, NoopAnimationsModule]
+      }).compileComponents();
+      fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      component = fixture.componentInstance;
+
+      loader = TestbedHarnessEnvironment.loader(fixture);
+    })
+  );
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'ingress-fs-finder'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ingress-fs-finder');
+  it(`should have as title 'Ingress First Saturday Finder'`, () => {
+    expect(component.title).toEqual('Ingress First Saturday Finder');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ingress-fs-finder app is running!');
+  describe('The User Interface', () => {
+    it('should render title', () => {
+      const compiled = fixture.nativeElement;
+      expect(compiled.querySelector('mat-toolbar span').textContent).toContain(
+        component.title
+      );
+    });
+
+    it('should have London preselected', async () => {
+      const timezones = await loader.getHarness<MatSelectHarness>(
+        MatSelectHarness.with({ selector: '#timezone' })
+      );
+      expect(await timezones.getValueText()).toEqual('Europe/London');
+    });
   });
 });
