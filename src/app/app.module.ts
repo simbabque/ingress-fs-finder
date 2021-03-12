4776e6fa62487ee0034491af5a7bf8b9ef018d35
Julien Fiegehenn <simbabque@cpan.org>
2021-03-12 21:58:37 +0000

    get events via http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { EventsComponent } from './events/events.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EventsComponent
-  ],
+  declarations: [AppComponent, EventsComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    HttpClientModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -1,25 +1,65 @@
 import { Injectable } from '@angular/core';
 import { Event } from './event';
 import { Observable, of } from 'rxjs';
-import * as data from '../../events.json';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+interface IEventResponseEvent {
+  datetime_local: string;
+  lat: number;
+  link: string;
+  location: string;
+  lon: number;
+  time_local: string;
+  timezone: string;
+}
+interface IEventResponse {
+  events: IEventResponseEvent[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class EventService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  private eventsUrl = 'events.json';
+
+  /**
+   * Get events from our own events.json file.
+   */
   getEvents(): Observable<Event[]> {
-    return of(data.events
-      .map((e) => {
-        let datetime = new Date(e.datetime_local);
-        delete e.datetime_local;
-        return new Event({ ...e, ...{ datetime: datetime } });
-      })
-      .sort(
-        (a, b) =>
-          <any>a.datetime - <any>b.datetime ||
-          a.location.localeCompare(b.location)
-      ));
+    return this.http.get(this.eventsUrl).pipe(
+      map(
+        (data: IEventResponse) =>
+          data.events
+            .map((e: IEventResponseEvent) => {
+              let datetime = new Date(e.datetime_local);
+              delete e.datetime_local;
+              return new Event({ ...e, ...{ datetime: datetime } });
+            })
+            .sort(
+              (a, b) =>
+                <any>a.datetime - <any>b.datetime ||
+                a.location.localeCompare(b.location)
+            ),
+        catchError(this.handleError<Event[]>('getEvents', []))
+      )
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
