a83519366d6c02b8cdbc55641188d2d5af2fb551
Julien Fiegehenn <simbabque@cpan.org>
2021-03-28 21:58:52 +0100

    mark past events greyed out and with tooltip #7

--- src/app/events/events.component.spec.ts
+++ src/app/events/events.component.spec.ts
@@ -5,9 +5,11 @@ import { EventsComponent } from './events.component';
 import { Event } from '../event';
 import { MatCardHarness } from '@angular/material/card/testing';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule, MatTooltip } from '@angular/material/tooltip';
+import { MatTooltipHarness } from '@angular/material/tooltip/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser'; 
 
 describe('EventsComponent', () => {
   let component: EventsComponent;
@@ -18,10 +20,8 @@ describe('EventsComponent', () => {
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [EventsComponent],
-        imports: [MatCardModule],
-        providers: [
-          EventsComponent
-        ],
+        imports: [MatCardModule, MatTooltipModule, NoopAnimationsModule],
+        providers: [EventsComponent],
         schemas: [CUSTOM_ELEMENTS_SCHEMA],
       }).compileComponents();
       fixture = TestBed.createComponent(EventsComponent);
@@ -35,7 +35,7 @@ describe('EventsComponent', () => {
   });
 
   describe('The User Interface', () => {
-    let card: MatCardHarness;
+    let cards: MatCardHarness[];
     beforeEach(async () => {
       fixture.detectChanges();
       component.timezone = 'Europe/London';
@@ -49,8 +49,17 @@ describe('EventsComponent', () => {
           time_local: '08:00 pm',
           timezone: 'Europe/London',
         }),
+        new Event({
+          datetime: new Date().setDate(new Date().getDate() + 7),
+          lat: '51.207015',
+          link: 'http://example.org',
+          location: 'Andover, United Kingdom',
+          lon: '-1.479153',
+          time_local: '08:00 pm',
+          timezone: 'Europe/London',
+        }),
       ];
-      card = await loader.getHarness<MatCardHarness>(MatCardHarness);
+      cards = await loader.getAllHarnesses<MatCardHarness>(MatCardHarness);
     });
 
     it('should link to the event page', async () => {
@@ -60,12 +69,46 @@ describe('EventsComponent', () => {
     });
 
     it('should display the day and time in the correct timezone', async () => {
-      expect(await card.getTitleText()).toEqual('Saturday 20:00');
+      expect(await cards[0].getTitleText()).toEqual('Saturday 20:00');
     });
 
     it('should change date and time when the timezone changes', async () => {
       component.timezone = 'Europe/Berlin';
-      expect(await card.getTitleText()).toEqual('Saturday 21:00');
+      expect(await cards[0].getTitleText()).toEqual('Saturday 21:00');
+    });
+
+    it('should mark the past event as past', async () => {
+      const card = fixture.nativeElement.querySelector('mat-card');
+      expect(card).toHaveClass('past-event');
+    });
+
+    it('should not mark the future event as past', async () => {
+      const domCards = fixture.nativeElement.querySelectorAll('mat-card');
+      expect(domCards[1]).not.toHaveClass('past-event');
+    });
+
+    it('should display tootltip on past events', async () => {
+      const tooltips = await loader.getAllHarnesses(MatTooltipHarness);
+      expect(tooltips.length).toBe(2);
+
+      expect(
+        await (await tooltips[0].host()).hasClass('past-event')
+      ).toBeTrue();
+      expect(
+        await (await tooltips[1].host()).hasClass('past-event')
+      ).toBeFalse();
+
+      await tooltips[0].show();
+      expect(await tooltips[0].getTooltipText()).toEqual(
+        'This event has already happened'
+      );
+
+      const ttDebugElements = fixture.debugElement.queryAll(By.css('.event-card'));
+      const pastEventTooltip = ttDebugElements[0].injector.get<MatTooltip>(MatTooltip);
+      const futureEventTooltip = ttDebugElements[1].injector.get<MatTooltip>(MatTooltip);
+
+      expect(pastEventTooltip.disabled).toBeFalse();
+      expect(futureEventTooltip.disabled).toBeTrue();
     });
   });
 });
